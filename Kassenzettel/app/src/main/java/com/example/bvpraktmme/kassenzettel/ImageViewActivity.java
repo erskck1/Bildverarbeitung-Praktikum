60871d9f78dcad2fed2b066273958958bba80376
Ersoy <ersoykocak1@gmail.com>
2017-05-14 17:38:13 +0200

    select image from gallery

---
+++ Kassenzettel/app/src/main/java/com/example/bvpraktmme/kassenzettel/ImageViewActivity.java
@@ -0,0 +1,40 @@
+package com.example.bvpraktmme.kassenzettel;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+public class ImageViewActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image_view);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        Bundle extras = getIntent().getExtras();
+        Uri imageUri = null;
+        if (extras != null && extras.containsKey("imageUri")) {
+             imageUri= Uri.parse(extras.getString("imageUri"));
+        }
+
+        Bitmap bm = null;
+        if (imageUri != null) {
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(),imageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        imageView.setImageBitmap(bm);
+    }
+}
--- Kassenzettel/app/src/main/java/com/example/bvpraktmme/kassenzettel/MainActivity.java
+++ Kassenzettel/app/src/main/java/com/example/bvpraktmme/kassenzettel/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.bvpraktmme.kassenzettel;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int REQUEST_CAMERA = 0, SELECT_FILE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button takePictureButton = (Button) findViewById(R.id.take_picture);
         takePictureButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,15 +25,43 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        Button btnSelect = (Button) findViewById(R.id.select_gallery);
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);//
+                startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_FILE);
+            }
+        });
+
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-    // Used to load the 'native-lib' library on application startup.
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK ) {
+            if (requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+            else if (requestCode == REQUEST_CAMERA) {
+                //onCaptureImageResult(data);
+            }
+
+        }
+    }
+
+    private void onSelectFromGalleryResult(Intent data) {
+        Intent intent = new Intent(getApplicationContext(), ImageViewActivity.class);
+        intent.putExtra("imageUri", data.getData().toString());
+        startActivity(intent);
+    }
+
+
     static {
         System.loadLibrary("native-lib");
     }
