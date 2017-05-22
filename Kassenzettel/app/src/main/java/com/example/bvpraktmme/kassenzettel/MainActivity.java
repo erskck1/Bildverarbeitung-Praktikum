0ebd51c5c8e17d66394c764810b131e95bd85d62
SirHerr <marius.herr@icloud.com>
2017-05-22 16:28:42 +0200

    OpenCV Loading

--- Kassenzettel/app/src/main/java/com/example/bvpraktmme/kassenzettel/MainActivity.java
+++ Kassenzettel/app/src/main/java/com/example/bvpraktmme/kassenzettel/MainActivity.java
@@ -5,12 +5,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import org.opencv.android.OpenCVLoader;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    static {
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV not loaded");
+        } else {
+            Log.d(TAG, "OpenCV loaded");
+        }
+    }
 
     private int REQUEST_CAMERA = 0, SELECT_FILE = 1;
 
